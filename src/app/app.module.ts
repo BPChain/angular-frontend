a86460e756b12b738be0fa9f26650e2d7f995ba5
Svitlana Kalancha <svitlana.kalancha@student.hpi.de>
2017-12-13 11:11:28 +0100

    Fix spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { MetricComponent } from './dashboard/metric/metric.component';
 import { FormComponent } from './dashboard/form/form.component';
 import { PublicChartsComponent } from './dashboard/barcharts/public-charts/public-charts.component';
 import { PrivateChartsComponent } from './dashboard/barcharts/private-charts/private-charts.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
--- src/app/services/private-stats.service.ts
+++ src/app/services/private-stats.service.ts
@@ -1,6 +1,6 @@
-import {Inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../../environments/environment';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class PrivateStatsService {
