c6b2670602866f912fa5c7c6ac8015aadf0d4515
Svitlana Kalancha <svitlana.kalancha@student.hpi.de>
2018-01-10 16:48:47 +0100

    Create validation to data convertation

--- src/app/services/date-builder.service.ts
+++ src/app/services/date-builder.service.ts
@@ -6,8 +6,12 @@ export class DateBuilderService {
   constructor() { }
 
   public convertDate(day:Date, hours: number, minutes: number) {
+    if (hours != null) {
     day.setHours(hours);
+    }
+    if (minutes != null) {
     day.setMinutes(minutes);
+    }
     return day;
   }
 }
