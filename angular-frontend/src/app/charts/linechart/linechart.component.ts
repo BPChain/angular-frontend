b757824f0a9f6a8315105bcc8fb5035296aae1b5
Tom Lichtenstein <tomli96@web.de>
2018-03-07 15:20:25 +0100

    Encapsulate linechart logic to seperate component

--- angular-frontend/src/app/charts/linechart/linechart.component.ts
+++ angular-frontend/src/app/charts/linechart/linechart.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {DataRetrieverService} from '../../services/data-retriever.service';
 
 @Component({
   selector: 'app-linechart',
@@ -7,98 +8,208 @@ import {Component, Input, OnChanges, OnInit} from '@angular/core';
 })
 export class LinechartComponent implements OnInit, OnChanges {
 
-  @Input() labels: string[];
-  @Input() chainData: any[];
+  @Input() selectedChains: object;
 
-  private lineChartType: string;
-  private lineChartLegend: boolean;
-  private lineChartOptions: any;
-  private lineChartColors: Array<any>;
-  private lineChartLabels: Array<any>;
+  // Chart.js parameter
   private lineChartData: Array<any>;
-  private chartReady = false;
+  private lineChartLabels: Array<any>;
+  private lineChartOptions: object;
+  private lineChartColors: Array<any>;
+  private lineChartLegend: boolean;
+  private lineChartType: string;
 
-  constructor() {
-    this.lineChartData = [{data: [1,2,2,1,4], label: 'Ethereum'},{data: [11,2,22,1,4], label: 'Xain'}];
-    this.lineChartLabels = [1,2,3,4,5];
-    this.chainData = [];
-    this.lineChartType = 'line';
-    this.lineChartLegend = true;
+  // Chain data
+  private datasetStore: Array<any>;
+  private datasets: object;
+
+  // Controll chart reload
+  private display: boolean;
+  private refresh: boolean;
+
+  constructor(private _dataRetriever: DataRetrieverService) {
+    this.selectedChains = this.initDatasets();
+  }
+
+  private initializeChart() {
+    this.datasets = this.initDatasets();
+    this.display = true;
+    this.refresh = false;
+    this.lineChartLabels = [
+      '1', '2', '3', '4', '5', '6', '7', '8',
+      '9', '10', '11', '12', '13', '14', '15',
+    ];
+    this.lineChartData = this.getDefaultDataset();
     this.lineChartOptions = {
       responsive: true,
       elements: { point: { radius: 0 } },
     };
-  this.lineChartColors = [
+    this.lineChartColors = [
+      { // blue
+        backgroundColor: 'rgba(24, 108, 187, 0.2)',
+        borderColor: 'rgb(24, 108, 187)',
+        pointBackgroundColor: 'rgb(24, 108, 187)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(24, 108, 187,0.8)'
+      },
+      { // red
+        backgroundColor: 'rgba(228, 46, 46, 0.2)',
+        borderColor: 'rgb(228, 46, 46)',
+        pointBackgroundColor: 'rgb(228, 46, 46)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(228, 46, 46,1)'
+      },
+      { // green
+        backgroundColor: 'rgba(9, 211, 43, 0.2)',
+        borderColor: 'rgb(9, 211, 43)',
+        pointBackgroundColor: 'rgb(9, 211, 43)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(9, 211, 43,0.8)'
+      },
+      { // yellow
+        backgroundColor: 'rgba(250, 235, 27, 0.2)',
+        borderColor: 'rgb(250, 235, 27)',
+        pointBackgroundColor: 'rgb(250, 235, 27)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(250, 235, 27,0.8)'
+      },
+      { // purple
+        backgroundColor: 'rgba(155, 51, 216, 0.2)',
+        borderColor: 'rgb(155, 51, 216)',
+        pointBackgroundColor: 'rgb(155, 51, 216)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(155, 51, 216,0.8)'
+      },
+      { // orange
+        backgroundColor: 'rgba(250, 66, 20, 0.2)',
+        borderColor: 'rgb(250, 66, 20)',
+        pointBackgroundColor: 'rgb(250, 66, 20)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(250, 66, 20,0.8)'
+      },
       { // grey
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
+        backgroundColor: 'rgba(110, 110, 110, 0.2)',
+        borderColor: 'rgb(110, 110, 110)',
+        pointBackgroundColor: 'rgb(110, 110, 110)',
         pointBorderColor: '#fff',
         pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        pointHoverBorderColor: 'rgba(110, 110, 110,0.8)'
       },
-      {
-        backgroundColor: 'rgba(0,159,177,0.2)',
-        borderColor: 'rgba(148,159,0,1)',
-        pointBackgroundColor: 'rgba(148,0,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,0,0,0.8)'
-      },
-      {
-        backgroundColor: 'rgba(30,19,127,0.2)',
-        borderColor: 'rgba(108,133,0,1)',
-        pointBackgroundColor: 'rgba(248,98,77,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,0,0,0.8)'
-      },
-     ];
-
+    ];
+    this.lineChartLegend = true;
+    this.lineChartType = 'line';
   }
 
+  private compareObjects(a, b): number {
+    if (a['label'] < b['label']) {
+      return -1;
+    }
+    if (a['label'] > b['label']) {
+      return 1;
+    }
+    return 0;
+  }
+
+  public updateChart(datasets): void {
+    let combinedDatasets: Array<object>;
+
+    if (this.isEmptyDataset(datasets)) {
+      combinedDatasets = this.getDefaultDataset();
+    } else {
+      combinedDatasets = datasets['private'].map(chart => {
+        return {data: chart.data, label: `Private-${chart.label}`};
+      });
+      combinedDatasets = combinedDatasets.concat(
+        datasets['public'].map(chart => {
+          return {data: chart.data, label: `Public-${chart.label}`};
+      }));
+    }
+    this.lineChartData = combinedDatasets.sort(
+      (a, b) => this.compareObjects(a, b)
+    );
+    if (this.refresh) {
+      this.display = false;
+      this.refresh = false;
+      setTimeout(() => {
+        this.display = true;
+      }, 1);
+    }
+  }
+
+  private isEmptyDataset(dataset): boolean {
+    if (dataset['private'].length > 0) {
+      return false;
+    }
+    if (dataset['public'].length > 0) {
+      return false;
+    }
+    return true;
+  }
+
+  private updateDatasets(chainsToDisplay) {
+    this.datasets = this.initDatasets();
+    chainsToDisplay['private'].forEach(chain => {
+        this._dataRetriever
+          .getPrivateChainApiData(chain)
+          .subscribe(res => {
+            this.datasets['private'].push(res);
+          });
+      });
+      chainsToDisplay['public'].forEach(chain => {
+        this._dataRetriever
+          .getPublicChainApiData(chain)
+          .subscribe(res => {
+            this.datasets['public'].push(res);
+          });
+      });
+  }
+
+  private trackChartDataUpdates() {
+    setInterval(() => {
+      if (this.isEmptyDataset(this.selectedChains)) {
+        this.updateChart(this.initDatasets());
+      } else {
+        if (!(this.isEmptyDataset(this.datasets))) {
+          this.updateChart(this.datasets);
+        }
+        this.updateDatasets(this.selectedChains);
+      }
+    }, 1000);
+  }
+
+  private initDatasets(): object {
+    return {public: [], private: []};
+  }
+
+  private getDefaultDataset(): Array<object> {
+    return [
+      {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], label: 'No chain selected'}
+    ];
+  }
+
+  // Initialize
   ngOnInit() {
-
+    this.initializeChart();
+    this.trackChartDataUpdates();
   }
 
- ngOnChanges() {
-   if (this.chartReady) {
-    this.lineChartData = this.chainData;
-   }
- }
-    /*
-  this.lineChartData.length = 0;
-  if (this.chainData) {
-    this.lineChartData = this.chainData.forEach(chain => {
-      this.lineChartData.push(chain);
-    });
-  }
-    this.lineChartLabels = [1,2,3,4,5,6];
+  // Selection updates
+  ngOnChanges() {
+    this.refresh = true;
+    this.updateDatasets(this.selectedChains);
   }
 
- // [{data: this.chainData[0].data, label: this.chainData[0].label},{data: this.chainData[1].data, label: this.chainData[1].label}];
-
-
-   /*
-    this.lineChartData.length = 0;
-    this.lineChartLabels.length = 0;
-    this.chainData.forEach(chain => {
-      this.lineChartData.push(chain.data);
-    });
-    this.labels.forEach(label => {
-      this.lineChartLabels.push(label);
-    });
-    console.info('lineChartData: ', this.lineChartData);
-    console.info('lineChartLabels: ', this.lineChartLabels);
-  }*/
-
-
-  // events
+  // Events
   public chartClicked(e: any): void {
-    //console.log(e);
+    console.log(e);
   }
 
   public chartHovered(e: any): void {
-    //console.log(e);
+    console.log(e);
   }
 }
--- angular-frontend/src/app/data-visualization-bar/data-visualization-bar.component.ts
+++ angular-frontend/src/app/data-visualization-bar/data-visualization-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DataRetrieverService } from '../services/data-retriever.service';
 import { ChainSelectorService } from '../services/chain-selector.service';
+import { LinechartComponent } from '../charts/linechart/linechart.component';
 
 
 @Component({
@@ -9,113 +9,10 @@ import { ChainSelectorService } from '../services/chain-selector.service';
   styleUrls: ['./data-visualization-bar.component.scss']
 })
 export class DataVisualizationBarComponent implements OnInit {
-  public lineChartData: Array<any>;
-  public lineChartLabels: Array<any>;
-  public lineChartOptions: object;
-  public lineChartColors: Array<any>;
-  public lineChartLegend: boolean;
-  public lineChartType: string;
-
-  private datasetStore: Array<any>;
-  private datasets: object;
   private selectedChains: object;
 
-  private display: boolean;
-  private refresh: boolean;
+  constructor(private _chainSelector: ChainSelectorService) {
 
-  private compareObjects(a, b): number {
-    if (a['label'] < b['label']) {
-      return -1;
-    }
-    if (a['label'] > b['label']) {
-      return 1;
-    }
-    return 0;
-  }
-
-  public update(datasets): void {
-    let combinedDatasets: Array<object>;
-
-    if (this.isEmptyDataset(datasets)) {
-      combinedDatasets = this.initDefaultDataset();
-    } else {
-      combinedDatasets = datasets['private'].map(chart => {
-        return {data: chart.data, label: `Private-${chart.label}`};
-      });
-      combinedDatasets = combinedDatasets.concat(
-        datasets['public'].map(chart => {
-          return {data: chart.data, label: `Public-${chart.label}`};
-      }));
-    }
-    this.lineChartData = combinedDatasets.sort(
-      (a, b) => this.compareObjects(a, b)
-    );
-    if (this.refresh) {
-      this.display = false;
-      this.refresh = false;
-      setTimeout(() => {
-        this.display = true;
-      }, 1);
-    }
-  }
-
-  private initializeChart() {
-    this.datasets = this.initDataset();
-    this.display = true;
-    this.refresh = false;
-    this.selectedChains = this.initDataset();
-    this.lineChartLabels = [
-      '1', '2', '3', '4', '5', '6', '7', '8',
-      '9', '10', '11', '12', '13', '14', '15',
-    ];
-    this.lineChartData = this.initDefaultDataset();
-    this.lineChartOptions = {
-      responsive: true,
-      elements: { point: { radius: 0 } },
-    };
-    this.lineChartColors = [
-      { // grey
-        backgroundColor: 'rgba(255, 78, 78, 0.2)',
-        borderColor: 'rgb(255, 78, 78)',
-        pointBackgroundColor: 'rgb(255, 78, 78)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-      },
-      { // dark grey
-        backgroundColor: 'rgba(78, 90, 255, 0.2)',
-        borderColor: 'rgb(78, 90, 255)',
-        pointBackgroundColor: 'rgb(78, 90, 255)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(77,83,96,1)'
-      },
-      { // grey
-        backgroundColor: 'rgba(13, 158, 0, 0.2)',
-        borderColor: 'rgb(13, 158, 0)',
-        pointBackgroundColor: 'rgb(13, 158, 0)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-      }
-    ];
-    this.lineChartLegend = true;
-    this.lineChartType = 'line';
-  }
-
-  // events
-  public chartClicked(e: any): void {
-    console.log(e);
-  }
-
-  public chartHovered(e: any): void {
-    console.log(e);
-  }
-
-  constructor(
-    private _dataRetriever: DataRetrieverService,
-    private _chainSelector: ChainSelectorService
-  ) {
   }
 
   private equalsSelection(selectionA, selectionB): boolean {
@@ -127,69 +24,25 @@ export class DataVisualizationBarComponent implements OnInit {
     return false;
   }
 
-  private isEmptyDataset(dataset): boolean {
-    if (dataset['private'].length > 0) {
-      return false;
-    }
-    if (dataset['public'].length > 0) {
-      return false;
-    }
-    return true;
-  }
-
-  private initDataset(): object {
-    return {public: [], private: []};
-  }
-
-  private initDefaultDataset(): Array<object> {
-    return [
-      {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], label: 'No chain selected'}
-    ];
-  }
-
-  private updateDatasets(chainsToDisplay) {
-    this.datasets = this.initDataset();
-    chainsToDisplay['private'].forEach(chain => {
-        this._dataRetriever
-          .getPrivateChainApiData(chain)
-          .subscribe(res => {
-            this.datasets['private'].push(res);
-          });
-      });
-      chainsToDisplay['public'].forEach(chain => {
-        this._dataRetriever
-          .getPublicChainApiData(chain)
-          .subscribe(res => {
-            this.datasets['public'].push(res);
-          });
-      });
-    }
-
-  private trackData() {
+  private trackSelectionUpdates() {
     setInterval(() => {
       const chainsToDisplay = Object.assign(
         {},
         (this._chainSelector.getSelectedChains()),
       );
-
-      if (this.isEmptyDataset(chainsToDisplay)) {
-        this.update(this.initDataset());
-      } else {
-      if (!(this.isEmptyDataset(this.datasets))) {
-        this.update(this.datasets);
-      }
-      if (!(this.equalsSelection(chainsToDisplay, this.selectedChains))) {
-        this.refresh = true;
-      }
-        this.updateDatasets(chainsToDisplay);
+      if (!(this.equalsSelection(this.selectedChains, chainsToDisplay))) {
         this.selectedChains = chainsToDisplay;
       }
-    }, 200);
+
+    }, 500);
   }
 
+  private initDatasets(): object {
+    return {public: [], private: []};
+  }
 
   ngOnInit() {
-    this.initializeChart();
-    this.trackData();
+    this.selectedChains = this.initDatasets();
+    this.trackSelectionUpdates();
   }
 }
