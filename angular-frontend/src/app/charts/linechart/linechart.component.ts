77aa95ba6109c3e5e82e704e3ddc7638c225ffbd
Anton von Weltzien <anton.weltzien@gmail.com>
2018-03-11 23:12:33 +0100

    refactor large parts of linechart

--- angular-frontend/src/app/charts/linechart/linechart.component.ts
+++ angular-frontend/src/app/charts/linechart/linechart.component.ts
@@ -11,119 +11,118 @@ export class LinechartComponent implements OnInit, OnChanges {
   @Input() selectedChains: object;
   @Input() selectedParameter: string;
 
-  // Chart.js parameter
-  private lineChartData: Array<any>;
-  private lineChartLabels: Array<any>;
-  private lineChartOptions: object;
-  private lineChartColors: Array<any>;
-  private lineChartLegend: boolean;
-  private lineChartType: string;
 
-  // Chain data
-  private datasetStore: Array<any>;
-  private datasets: object;
+  // Chart.js parameter
+  private lineChart: {
+    data: Array<any>,
+    labels: Array<string>,
+    options: object,
+    colors: Array<any>,
+    showsLegend: boolean,
+    type: string
+  };
+
 
   // Controll chart reload
-  private display: boolean;
-  private refresh: boolean;
+  private display:boolean;
+  private refresh:boolean;
 
   constructor(private _dataRetriever: DataRetrieverService) {
-    this.selectedChains = this.initDatasets();
-    this.selectedParameter = '';
   }
 
   private initializeChart() {
-    this.datasets = this.initDatasets();
     this.display = true;
     this.refresh = false;
-    this.lineChartLabels = [
-      '1', '2', '3', '4', '5', '6', '7', '8',
-      '9', '10', '11', '12', '13', '14', '15',
-    ];
-    this.lineChartData = this.getDefaultDataset();
-    this.lineChartOptions = {
-      responsive: true,
-      elements: { point: { radius: 0 } },
+    this.lineChart = {
+      labels: [
+        '1', '2', '3', '4', '5', '6', '7', '8',
+        '9', '10', '11', '12', '13', '14', '15',
+      ],
+      data: [],
+      options: {
+        responsive: true,
+        elements: {point: {radius: 0}},
+      },
+      colors: [
+        { // blue
+          backgroundColor: 'rgba(24, 108, 187, 0.2)',
+          borderColor: 'rgb(24, 108, 187)',
+          pointBackgroundColor: 'rgb(24, 108, 187)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(24, 108, 187,0.8)'
+        },
+        { // red
+          backgroundColor: 'rgba(228, 46, 46, 0.2)',
+          borderColor: 'rgb(228, 46, 46)',
+          pointBackgroundColor: 'rgb(228, 46, 46)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(228, 46, 46,1)'
+        },
+        { // green
+          backgroundColor: 'rgba(9, 211, 43, 0.2)',
+          borderColor: 'rgb(9, 211, 43)',
+          pointBackgroundColor: 'rgb(9, 211, 43)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(9, 211, 43,0.8)'
+        },
+        { // yellow
+          backgroundColor: 'rgba(250, 235, 27, 0.2)',
+          borderColor: 'rgb(250, 235, 27)',
+          pointBackgroundColor: 'rgb(250, 235, 27)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(250, 235, 27,0.8)'
+        },
+        { // purple
+          backgroundColor: 'rgba(155, 51, 216, 0.2)',
+          borderColor: 'rgb(155, 51, 216)',
+          pointBackgroundColor: 'rgb(155, 51, 216)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(155, 51, 216,0.8)'
+        },
+        { // orange
+          backgroundColor: 'rgba(250, 66, 20, 0.2)',
+          borderColor: 'rgb(250, 66, 20)',
+          pointBackgroundColor: 'rgb(250, 66, 20)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(250, 66, 20,0.8)'
+        },
+        { // grey
+          backgroundColor: 'rgba(110, 110, 110, 0.2)',
+          borderColor: 'rgb(110, 110, 110)',
+          pointBackgroundColor: 'rgb(110, 110, 110)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(110, 110, 110,0.8)'
+        },
+        { // turquoise
+          backgroundColor: 'rgba(10, 190, 190, 0.2)',
+          borderColor: 'rgb(10, 190, 190)',
+          pointBackgroundColor: 'rgb(10, 190, 190)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(10, 190, 190,0.8)'
+        },
+        { // turquoise
+          backgroundColor: 'rgba(231, 0, 193, 0.2)',
+          borderColor: 'rgb(231, 0, 193)',
+          pointBackgroundColor: 'rgb(231, 0, 193)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(231, 0, 193,0.8)'
+        },
+      ],
+      showsLegend: true,
+      type: 'line',
     };
-    this.lineChartColors = [
-      { // blue
-        backgroundColor: 'rgba(24, 108, 187, 0.2)',
-        borderColor: 'rgb(24, 108, 187)',
-        pointBackgroundColor: 'rgb(24, 108, 187)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(24, 108, 187,0.8)'
-      },
-      { // red
-        backgroundColor: 'rgba(228, 46, 46, 0.2)',
-        borderColor: 'rgb(228, 46, 46)',
-        pointBackgroundColor: 'rgb(228, 46, 46)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(228, 46, 46,1)'
-      },
-      { // green
-        backgroundColor: 'rgba(9, 211, 43, 0.2)',
-        borderColor: 'rgb(9, 211, 43)',
-        pointBackgroundColor: 'rgb(9, 211, 43)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(9, 211, 43,0.8)'
-      },
-      { // yellow
-        backgroundColor: 'rgba(250, 235, 27, 0.2)',
-        borderColor: 'rgb(250, 235, 27)',
-        pointBackgroundColor: 'rgb(250, 235, 27)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(250, 235, 27,0.8)'
-      },
-      { // purple
-        backgroundColor: 'rgba(155, 51, 216, 0.2)',
-        borderColor: 'rgb(155, 51, 216)',
-        pointBackgroundColor: 'rgb(155, 51, 216)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(155, 51, 216,0.8)'
-      },
-      { // orange
-        backgroundColor: 'rgba(250, 66, 20, 0.2)',
-        borderColor: 'rgb(250, 66, 20)',
-        pointBackgroundColor: 'rgb(250, 66, 20)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(250, 66, 20,0.8)'
-      },
-      { // grey
-        backgroundColor: 'rgba(110, 110, 110, 0.2)',
-        borderColor: 'rgb(110, 110, 110)',
-        pointBackgroundColor: 'rgb(110, 110, 110)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(110, 110, 110,0.8)'
-      },
-      { // turquoise
-        backgroundColor: 'rgba(10, 190, 190, 0.2)',
-        borderColor: 'rgb(10, 190, 190)',
-        pointBackgroundColor: 'rgb(10, 190, 190)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(10, 190, 190,0.8)'
-      },
-      { // turquoise
-        backgroundColor: 'rgba(231, 0, 193, 0.2)',
-        borderColor: 'rgb(231, 0, 193)',
-        pointBackgroundColor: 'rgb(231, 0, 193)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(231, 0, 193,0.8)'
-      },
-    ];
-    this.lineChartLegend = true;
-    this.lineChartType = 'line';
   }
 
-  private compareObjects(a, b): number {
+  private alphabeticallyFirst(a, b): number {
     if (a['label'] < b['label']) {
       return -1;
     }
@@ -133,29 +132,17 @@ export class LinechartComponent implements OnInit, OnChanges {
     return 0;
   }
 
-  public updateChart(datasets: object, parameter: string): void {
-    let combinedDatasets: Array<object>;
 
-    if (this.isEmptyDataset(datasets) || parameter === 'none') {
-      combinedDatasets = this.getDefaultDataset();
-    } else {
-      combinedDatasets = datasets['private'].map(chart => {
-        return {
-          data: chart[parameter],
-          label: `Private-${chart['chainName']}`
-        };
-      });
-      combinedDatasets = combinedDatasets.concat(
-        datasets['public'].map(chart => {
-          return {
-            data: chart[parameter],
-            label: `Public-${chart['chainName']}`
-          };
-      }));
-    }
-    this.lineChartData = combinedDatasets.sort(
-      (a, b) => this.compareObjects(a, b)
-    );
+  public updateChart(dataset: any): void {
+    console.log(dataset)
+    const dataToDisplay = dataset.map(chain => ({
+      data: chain[this.selectedParameter],
+      label: chain['access'].concat('-', chain['chainName'])}));
+    console.log(dataToDisplay)
+    /*this.lineChart.data = combinedDatasets.sort(
+      (chainA, chainB) => this.alphabeticallyFirst(chainA, chainB)
+    );*/
+    this.lineChart.data = dataToDisplay;
     if (this.refresh) {
       this.display = false;
       this.refresh = false;
@@ -165,47 +152,17 @@ export class LinechartComponent implements OnInit, OnChanges {
     }
   }
 
-  private isEmptyDataset(dataset): boolean {
-    if (dataset['private'].length > 0) {
-      return false;
-    }
-    if (dataset['public'].length > 0) {
-      return false;
-    }
-    return true;
-  }
 
-  private updateDatasets(chainsToDisplay) {
-    this.datasets = this.initDatasets();
-    chainsToDisplay['private'].forEach(chain => {
-        this._dataRetriever
-          .getPrivateChainApiData(chain)
-          .subscribe(res => {
-            this.datasets['private'].push(res);
-          });
-      });
-      chainsToDisplay['public'].forEach(chain => {
-        this._dataRetriever
-          .getPublicChainApiData(chain)
-          .subscribe(res => {
-            this.datasets['public'].push(res);
-          });
-      });
+  private updateDataAndChart() {
+    console.log(this.selectedChains, this.selectedParameter)
+    const observable = this._dataRetriever.getChainData(this.selectedChains);
+    observable.subscribe(x => this.updateChart(x));
   }
 
   private trackChartDataUpdates() {
     setInterval(() => {
-      if (
-        this.isEmptyDataset(this.selectedChains) ||
-        this.selectedParameter === ''
-      ) {
-        this.updateChart(this.initDatasets(), 'none');
-      } else {
-        if (!(this.isEmptyDataset(this.datasets))) {
-          this.updateChart(this.datasets, this.selectedParameter);
-        }
-        this.updateDatasets(this.selectedChains);
-      }
+      console.log(this.lineChart);
+      this.updateDataAndChart();
     }, 5000);
   }
 
@@ -215,7 +172,10 @@ export class LinechartComponent implements OnInit, OnChanges {
 
   private getDefaultDataset(): Array<object> {
     return [
-      {data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], label: 'No chain selected'}
+      {
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        label: 'No chain selected'
+      }
     ];
   }
 
@@ -228,7 +188,7 @@ export class LinechartComponent implements OnInit, OnChanges {
   // Selection updates
   ngOnChanges() {
     this.refresh = true;
-    this.updateDatasets(this.selectedChains);
+    this.updateDataAndChart();
   }
 
   // Events
--- angular-frontend/src/app/services/data-retriever.service.ts
+++ angular-frontend/src/app/services/data-retriever.service.ts
@@ -1,22 +1,41 @@
-import { Inject, Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
 import 'rxjs/add/operator/map';
 
 
 @Injectable()
 export class DataRetrieverService {
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {
+  }
 
   getPublicChainApiData(chain) {
     return this._http
       .get(`http://localhost:3000/api/public/${chain.toLowerCase()}`)
-      .map(result => result);
+      .map(result => Object.assign(result, {access: 'public'}));
   }
 
   getPrivateChainApiData(chain) {
     return this._http
       .get(`http://localhost:3000/api/private/${chain.toLowerCase()}`)
-      .map(result => result);
+      .map(result => Object.assign(result, {access: 'private'}));
+  }
+
+  getChainData(selectedChains){
+    //Todo remove any with real type declaration
+    const responses$ : Array<Observable<any>> = [];
+    selectedChains['private'].forEach(chain => {
+      responses$.push(this.getPrivateChainApiData(chain));
+    });
+    selectedChains['public'].forEach(chain => {
+      responses$.push(this.getPublicChainApiData(chain));
+    });
+    console.log('vor return')
+    return Observable.forkJoin(...responses$)
   }
 }
+
+
+//Komplette abfrage, einfach alle chains übergeben und dann wait auf all
