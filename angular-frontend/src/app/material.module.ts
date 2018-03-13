97d59e0d0c5ca201202184cd30d0278d07e641ac
Tom Lichtenstein <tomli96@web.de>
2018-03-13 12:37:47 +0100

    Add scenario list that can select a scenario

--- angular-frontend/src/app/material.module.ts
+++ angular-frontend/src/app/material.module.ts
@@ -15,6 +15,7 @@ import {
   MatButtonToggleModule,
   MatDialogModule,
   MatSnackBarModule,
+  MatTooltipModule,
 } from '@angular/material';
 
 const modules = [
@@ -32,6 +33,7 @@ const modules = [
   MatButtonToggleModule,
   MatDialogModule,
   MatSnackBarModule,
+  MatTooltipModule,
 ];
 
 
--- angular-frontend/src/app/scenario-bar/scenario-bar.component.ts
+++ angular-frontend/src/app/scenario-bar/scenario-bar.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-scenario-bar',
   templateUrl: './scenario-bar.component.html',
   styleUrls: ['./scenario-bar.component.scss']
 })
-export class ScenarioBarComponent implements OnInit {
+export class ScenarioBarComponent {
 
-  constructor() { }
+  @Input() isAuthenticated: boolean;
+  private scenarios: Array<Object>;
+  private selectedScenario: string;
 
-  ngOnInit() {
+  constructor(public snackBar: MatSnackBar) {
+    this.scenarios = [
+      {name: 'EVAPCoin', description: 'This is a description.'},
+      {name: 'Scenario2', description: 'I like this coin.'},
+    ];
+    this.selectedScenario = '';
   }
 
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 2000,
+    });
+  }
+
+  selectScenario(scenario) {
+    if (this.isAuthenticated) {
+      if (this.selectedScenario === scenario) {
+        this.selectedScenario = '';
+        this.openSnackBar(`Successfully stopped scenario: ${scenario}`);
+      } else {
+        this.selectedScenario = scenario;
+        this.openSnackBar(`Successfully started scenario: ${scenario}`);
+      }
+    } else {
+      this.openSnackBar('You are not authorized');
+    }
+  }
 }
