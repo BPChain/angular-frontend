c35533cf8aabd6791b3bb6170eb658a50875ad9b
Tom Lichtenstein <tomli96@web.de>
2018-03-14 14:43:32 +0100

    Add config to store environment information

---
+++ angular-frontend/src/config.ts
@@ -0,0 +1,13 @@
+export const CONFIG = {
+  url: {
+    base: 'http://localhost:3000/',
+    login: 'login',
+    logout: 'logout',
+    checkLogin: 'checkLogin',
+    publicChain: 'api/public/',
+    privateChain: 'api/private/',
+    changeParameter: 'api/change',
+    connectedNodes: 'api/connectedNodes'
+  }
+};
+
