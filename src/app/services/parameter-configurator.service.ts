53c4fd0f9e71f7fea419aeae821002dc9d36d3ab
Tom Lichtenstein <tomli96@web.de>
2018-03-29 16:27:39 +0200

    Generate the parameter configuration dynamically

--- src/app/services/parameter-configurator.service.ts
+++ src/app/services/parameter-configurator.service.ts
@@ -32,7 +32,37 @@ export class ParameterConfiguratorService {
     .post(
       CONFIG.url.base + CONFIG.url.changeParameter,
       parameters,
-      {responseType: 'text'},
+      {responseType: 'text', withCredentials: true},
     );
+  }
+
+  startChain(chainName: string, target: string): Observable<string> {
+    return this.setChainParameters({
+      chainName,
+      target,
+      parameters: {
+        startChain: true,
+      }
+    });
+  }
+
+  stopChain(chainName: string, target: string): Observable<string> {
+    return this.setChainParameters({
+      chainName,
+      target,
+      parameters: {
+        stopChain: true,
+      }
+    });
+  }
+
+  switchChainTo(chainToStop: string, chainToStart: string, target: string): Observable<string> {
+    return this.setChainParameters({
+      chainName: chainToStop,
+      target,
+      parameters: {
+        switchChain: chainToStart,
+      }
+    });
   }
 }
