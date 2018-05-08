cb492ba80764e95f97c62054e8c282666f9e155a
bisbaldi <annika.baldi@googlemail.com>
2018-05-08 14:57:52 +0200

    Add timer to recording

--- src/app/chain-recorder/chain-recorder.component.ts
+++ src/app/chain-recorder/chain-recorder.component.ts
@@ -9,17 +9,24 @@ import {RecordingService} from '../services/recording.service';
 export class ChainRecorderComponent implements OnInit {
 
 
-  public isRecording: Boolean;
-  public fileName: String;
+  public isRecording: Boolean = false;
+  public creationDate: number = null;
+  public recordingTime: Object = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+  public recordingName: String;
   @Input() isAuthenticated: Boolean;
 
   constructor(private _recorder: RecordingService) { }
 
   onStartedRecording() {
-    this._recorder.startRecording(this.fileName)
+    this._recorder.startRecording(this.recordingName)
       .subscribe(result => {
         if (result === 'OK') {
           this.isRecording = true;
+          this.creationDate = Date.now();
         }
         console.info(result);
       });
@@ -30,6 +37,7 @@ export class ChainRecorderComponent implements OnInit {
       .subscribe(result => {
         if (result === 'OK') {
           this.isRecording = false;
+          this.creationDate = null;
         }
         console.info(result);
       });
@@ -39,9 +47,29 @@ export class ChainRecorderComponent implements OnInit {
   ngOnInit() {
     this._recorder.isRecording()
       .subscribe(result => {
-          this.isRecording = result === 'true';
+        console.info(result);
+          this.isRecording = result['isRecording'];
+          if (this.isRecording) {
+            this.creationDate = result['creationDate'];
+            this.recordingName = result['recordingName'];
+          } else {
+            this.creationDate = null;
+          }
           console.info(this.isRecording);
       });
+    this.calculateTime();
+  }
+
+  calculateTime() {
+    setInterval(() => {if (this.isRecording) {
+      let timeDelta = (Date.now() - this.creationDate) / 1000;
+      this.recordingTime['hours'] = Math.floor(timeDelta / 3600);
+      timeDelta = timeDelta - this.recordingTime['hours'] * 3600;
+      this.recordingTime['minutes'] = Math.floor( timeDelta / 60);
+      timeDelta = timeDelta - this.recordingTime['minutes'] * 60;
+      this.recordingTime['seconds'] = Math.floor(timeDelta);
+
+    }}, 1000);
   }
 
 }
